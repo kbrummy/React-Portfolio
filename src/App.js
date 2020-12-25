85840518a8d613423ea64f49f9daa69fa31a9095
Kristin Brumfield <kristinbrumfield@Kbrummy.local>
2020-12-24 21:41:47 -0800

    git page troubles still

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
+import CssBaseline from "@material-ui/core/CssBaseline";
 import "./App.css";
 import Home from "./Components/Home";
 import Projects from "./Components/Projects";
@@ -7,11 +8,13 @@ import Contact from "./Components/Contact";
 
 function App() {
   return (
-    <main>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/projects" component={Projects} />
-      <Route exact path="/contact" component={Contact} />
-    </main>
+    <CssBaseline>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/projects" component={Projects} />
+        <Route exact path="/contact" component={Contact} />
+      </Switch>
+    </CssBaseline>
   );
 }
 
